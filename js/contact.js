29fa5eadbfb8651d6011a941b0fcf728f1da954d
Myhome210190 <myhome210190@gmail.com>
2022-12-09 13:16:47 +0000

    Removed bootstrap and jquery validation library

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+/*
+
+Form validation
+
+name input:
+- is required
+- needs at least 2 characters
+
+email input:
+- is required
+- needs to be a valid email
+
+subject select:
+- is required
+
+message input:
+- is required
+- maximum 500 characters
+
+*/
