3cb6e7856e1ad72e738983d8f7335139924a3bc4
Myhome210190 <myhome210190@gmail.com>
2022-12-10 00:02:43 +0000

    finished contact us page and added validation to the form

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,115 @@
-/*
+// When the document is ready.
+$(document).ready(function () {
+  // store reference of element "submit-btn"
+  const submitBtn = document.getElementById('submit-btn');
 
-Form validation
+  //declare validation function on event
+  const validate = (e) => {
+    // prevent default behavior.
+    e.preventDefault();
 
-name input:
-- is required
-- needs at least 2 characters
+    // store references of label input
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const subject = document.getElementById('subject');
+    const message = document.getElementById('message');
+    const validationMessage = document.getElementById('validation-message');
 
-email input:
-- is required
-- needs to be a valid email
+    // email regular expression. (valid email example: a@a.aa)
+    const emailRegex =
+      /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-subject select:
-- is required
+    // check if name is not empty
+    if (name.value === '') {
+      validationMessage.innerHTML = 'Please enter your name';
+      validationMessage.style.display = 'block';
+      name.focus();
+      // console fail message
+      console.log('#name validation failed');
+      return;
+    }
+    //check if name is not shorter then 2 characters
+    else if (name.value.length < 2) {
+      validationMessage.innerHTML = 'Name should be at least 2 characters';
+      validationMessage.style.display = 'block';
+      name.focus();
+      // console fail message
+      console.log('#name validation failed');
+      return;
+    }
 
-message input:
-- is required
-- maximum 500 characters
+    //check if email is not empty
+    if (email.value === '') {
+      validationMessage.innerHTML = 'Please enter your email';
+      validationMessage.style.display = 'block';
+      email.focus();
+      // console fail message
+      console.log('#email validation failed');
+      return;
+    }
+    //check if email is valid
+    else if (emailRegex.test(email.value) == false) {
+      validationMessage.innerHTML = 'Email is not valid';
+      validationMessage.style.display = 'block';
+      email.focus();
+      // console fail message
+      console.log('#email validation failed');
+      return;
+    }
 
-*/
+    //check if option is selected
+    if (subject.value === '') {
+      validationMessage.innerHTML = 'Please select an option';
+      validationMessage.style.display = 'block';
+      subject.focus();
+      // console fail message
+      console.log('#select validation failed');
+      return;
+    }
+
+    // check if the message is not empty
+    if (message.value === '') {
+      validationMessage.innerHTML = 'Please enter your message';
+      validationMessage.style.display = 'block';
+      message.focus();
+      // console fail message
+      console.log('#message validation failed');
+      return;
+    }
+    // check if message is not longer then 500 characters
+    else if (message.value.length > 500) {
+      validationMessage.innerHTML =
+        'Message must must have maximum 500 characters';
+      validationMessage.style.display = 'block';
+      message.focus();
+      // console fail message
+      console.log('#message validation failed');
+      return;
+    }
+
+    // display success alert message
+    alert(
+      'Your message has been sent!\n\nName: ' +
+        name.value +
+        '\nEmail: ' +
+        email.value +
+        '\nSubject: ' +
+        subject.value +
+        '\nMessage: ' +
+        message.value
+    );
+    // console success message
+    console.log('Message sent successfully');
+
+    // reset form and validation message
+    name.value = '';
+    email.value = '';
+    subject.value = '';
+    message.value = '';
+    validationMessage.innerHTML = '';
+    validationMessage.style.display = 'none';
+  };
+
+  // add click event listener to #submit-btn
+  submitBtn.addEventListener('click', validate);
+});
